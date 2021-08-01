3cc2ad7e89944aee669c0f99a32a8acc00428641
Tony Benoy <8504669+tonybenoy@users.noreply.github.com>
2021-08-01 21:13:12 +0000

    structoptwip

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 mod aur;
 mod func;
 mod settings;
-use settings::Config;
+use structopt::StructOpt;
+// use settings::Config;
 use std::env;
 use std::error::Error;
 use std::process;
 fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
-    let resp = config.run();
-    println!("{:#?}", resp);
+    // let args: Vec<String> = env::args().collect();
+    // let config = Config::new(&args).unwrap_or_else(|err| {
+    //     println!("Problem parsing arguments: {}", err);
+    //     process::exit(1);
+    // });
+    // let resp = config.run();
+    // println!("{:#?}", resp);
+    let opt = settings::Opt::from_args();
+    println!("{:?}", opt);
     Ok(())
 }
--- src/settings.rs
+++ src/settings.rs
@@ -1,80 +1,96 @@
 use crate::aur::{AurPkg, AurResponse};
 use std::error::Error;
-use std::str::FromStr;
 use std::string::ToString;
+use structopt::StructOpt;
 use strum_macros::{EnumString, ToString};
-
-pub struct Config {
-    functionality: Functionality,
-    query_by: String,
-    query: String,
+#[derive(Debug, StructOpt)]
+#[structopt(name = "raur", about = "A simple AUR helper")]
+pub struct Opt {
+    #[structopt(subcommand)]
+    cmd: Command,
 }
 
-impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
-        let _type = args[1].clone();
-        let functionality = Functionality::from_str(&_type);
-        if functionality.is_err() {
-            return Err("functionality not available");
-        }
-        let functionality = functionality.unwrap();
-        functionality.validate(&args[2..args.len()]);
-
-        let query = args[3].clone();
-        let query_by = args[2].clone();
-        Ok(Config {
-            functionality,
-            query,
-            query_by,
-        })
-    }
-    pub fn run(&self) -> Result<AurResponse, reqwest::Error> {
-        let client = reqwest::blocking::Client::new();
-        let resp = client
-            .get("https://aur.archlinux.org/rpc/")
-            .query(&[
-                ("v", "5"),
-                ("type", &self.functionality.to_string()),
-                ("by", &self.query_by),
-                ("arg", &self.query),
-            ])
-            .send()?;
-        let val: AurResponse = serde_json::from_str(&resp.text()?).unwrap();
-        Ok(val)
-    }
+#[derive(Debug, StructOpt)]
+enum Command {
+    search { search_on: SearchParams },
+    info { pkgs: Vec<String> },
 }
-
-#[derive(Debug, EnumString, ToString)]
-pub enum Functionality {
-    #[strum(serialize = "search", serialize = "s")]
-    Search,
-    #[strum(serialize = "info", serialize = "i")]
-    Info,
-    #[strum(disabled)]
-    Install,
+#[derive(Debug, StructOpt)]
+struct SearchParams {
+    name: String,
+    maintainer: String,
 }
+// pub struct Config {
+//     functionality: Functionality,
+//     query_by: String,
+//     query: String,
+// }
 
-trait Validations {
-    fn validate(&self, arguments: &[String]) -> Result<(), Box<dyn Error>>;
-}
+// impl Config {
+//     pub fn new(args: &[String]) -> Result<Config, &str> {
+//         let _type = args[1].clone();
+//         let functionality = Functionality::from_str(&_type);
+//         if functionality.is_err() {
+//             return Err("functionality not available");
+//         }
+//         let functionality = functionality.unwrap();
+//         functionality.validate(&args[2..args.len()]);
 
-impl Validations for Functionality {
-    fn validate(&self, arguments: &[String]) -> Result<(), Box<dyn Error>> {
-        match self {
-            Functionality::Search => {
-                if arguments.len() < 4 {
-                    return Err("not enough arguments".into());
-                } else {
-                    Ok(())
-                }
-            }
-            Functionality::Info => {
-                if arguments.len() < 3 {
-                    return Err("not enough arguments".into());
-                }
-                Ok(())
-            }
-            Functionality::Install => Ok(()),
-        }
-    }
-}
+//         let query = args[3].clone();
+//         let query_by = args[2].clone();
+//         Ok(Config {
+//             functionality,
+//             query,
+//             query_by,
+//         })
+//     }
+//     pub fn run(&self) -> Result<AurResponse, reqwest::Error> {
+//         let client = reqwest::blocking::Client::new();
+//         let resp = client
+//             .get("https://aur.archlinux.org/rpc/")
+//             .query(&[
+//                 ("v", "5"),
+//                 ("type", &self.functionality.to_string()),
+//                 ("by", &self.query_by),
+//                 ("arg", &self.query),
+//             ])
+//             .send()?;
+//         let val: AurResponse = serde_json::from_str(&resp.text()?).unwrap();
+//         Ok(val)
+//     }
+// }
+
+// #[derive(Debug, EnumString, ToString)]
+// pub enum Functionality {
+//     #[strum(serialize = "search", serialize = "s")]
+//     Search,
+//     #[strum(serialize = "info", serialize = "i")]
+//     Info,
+//     #[strum(disabled)]
+//     Install,
+// }
+
+// trait Validations {
+//     fn validate(&self, arguments: &[String]) -> Result<(), Box<dyn Error>>;
+// }
+
+// impl Validations for Functionality {
+//     fn validate(&self, arguments: &[String]) -> Result<(), Box<dyn Error>> {
+//         match self {
+//             Functionality::Search => {
+//                 if arguments.len() < 4 {
+//                     return Err("not enough arguments".into());
+//                 } else {
+//                     Ok(())
+//                 }
+//             }
+//             Functionality::Info => {
+//                 if arguments.len() < 3 {
+//                     return Err("not enough arguments".into());
+//                 }
+//                 Ok(())
+//             }
+//             Functionality::Install => Ok(()),
+//         }
+//     }
+// }
